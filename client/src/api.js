51d5c296c493271c2d4b5c7cfb7099b33450e4f7
ahme-r <ehmar07@gmail.com>
2026-02-25 17:17:37 +0500

    fix: updated API URL for Create React App

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://intern-tracker-production.up.railway.app/api';
+const API_URL = 'https://intern-tracker-production.up.railway.app/api/interns';
 
 const api = axios.create({
     baseURL: API_URL
